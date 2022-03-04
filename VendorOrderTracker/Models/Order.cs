036177716d63b42c5e258cd0a79aefb521d0915f
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-04 09:49:52 -0800

    add 'set;' and pass Set Order Date test.

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -7,7 +7,7 @@ namespace VendorOrderTracker.Models
     public string Title { get; set; }
     public string Description { get; set; }
     public int Price { get; set; }
-    public string Date { get; }
+    public string Date { get; set; }
     public Order(string title, string description, int price, string date)
     {
       Title = title;
