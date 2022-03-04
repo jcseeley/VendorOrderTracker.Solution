3c098fb17af3d7ef4321cccddb3905270799ad6c
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-04 09:01:07 -0800

    add 'set;' and pass Set test.

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -4,7 +4,7 @@ namespace VendorOrderTracker.Models
 {
   public class Order
   {
-    public string Title { get; }
+    public string Title { get; set; }
     public Order(string title)
     {
       Title = title;
