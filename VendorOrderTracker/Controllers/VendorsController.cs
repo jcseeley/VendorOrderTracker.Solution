09e65399947ef169ba164b814dad11d0898f124a
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-04 14:35:14 -0800

    WIP: trying to get info to display correctly.

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
 
 namespace VendorOrderTracker.Controllers
 {
@@ -7,7 +10,39 @@ namespace VendorOrderTracker.Controllers
     [HttpGet("/vendors")]
     public ActionResult Index()
     {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
       return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("Vendor", selectedVendor);
+      model.Add("Orders", vendorOrders);
+      return View(model);
+    }
+
+    [HttpPost("/vendors/delete")]
+    public ActionResult Delete()
+    {
+    Vendor.ClearAll();
+    return View();
     }
   }
 }
