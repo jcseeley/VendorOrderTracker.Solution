970fa62d79f098827d07d4c22a38a2b4dbe70c68
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-04 08:29:22 -0800

    add base files and folders.

---
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System; 
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+
+namespace Order.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    
+    // [TestMethod]
+    // public void ClassConstructor_CreatesInstanceOfClass_Object()
+    // {
+    //   Class newObject = new Class("test");
+    //   Assert.AreEqual(typeof(Class), newObject.GetType()); 
+    // }
+  }
+}
+
+// [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+// }
