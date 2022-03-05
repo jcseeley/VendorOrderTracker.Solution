65ba96157c146a7ab1db6e4196a329db691f0495
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-04 16:23:42 -0800

    add OrdersController.

--- VendorOrderTracker/Controllers/HomeController.cs
+++ VendorOrderTracker/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
 
 namespace VendorOrderTracker.Controllers
 {
---
+++ VendorOrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{id}/orders/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+  }
+}
